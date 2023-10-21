7b73d338ebe5db5e7aa82d2eefa4ed65827dbfcb
Timmy Cole <greyhorror30@gmail.com>
2023-10-21 22:43:49 +0630

    Initial Commit

---
+++ MonthlyExpenseTracker/MonthlyExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyExpenseTrackerApp.swift
+//  MonthlyExpenseTracker
+//
+//  Created by Andrew Hardin on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
