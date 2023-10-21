f8c400e720b8c25d5e4140dee79a8749b9c02462
Timmy Cole <greyhorror30@gmail.com>
2023-10-21 22:47:14 +0630

    initial commit

--- MonthlyExpenseTracker/ContentView.swift
+++ MonthlyExpenseTracker/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Monthly Expenes Tracker")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
         }
         .padding()
     }
